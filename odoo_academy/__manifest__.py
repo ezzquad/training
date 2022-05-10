efa75cde7ac60ec649d20698707c612cbbc27485
Eligio Chan <ceo@ezzquad.com>
2022-05-10 15:58:48 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,28 @@
+# _*_ coding: utf-8 _*_
+
+
+{
+    "name": "Odoo Academy",
+    "summary": "Academy app to manage Training",
+    "description": """
+        Academy Module to manage training:
+        - Courses
+        - Session
+        - Attendees
+    """,
+    "author": "Ezzquad",
+    "website": "https://ezzquad.com",
+    "category": "Training",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        
+    ],
+    "demo": [
+        
+        
+    ],
+    "license": "GPL2"
+}
+
+
