7bf41c70f472c083c4c969263c569b0d3ce61413
Eligio Chan <ceo@ezzquad.com>
2022-05-10 18:43:21 +0000

    [ADD] Commit initial

---
+++ ezzquad_library/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,26 @@
+# _*_ coding: utf-8 _*_
+
+
+{
+    "name": "Ezzquad library",
+    "summary": "Library app",
+    "description": """
+       Library app
+    """,
+    "author": "Ezzquad",
+    "website": "https://ezzquad.com",
+    "category": "Training",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        
+    ],
+    "demo": [
+        
+        
+    ],
+    "license": "OPL-1"
+}
+
+
+
---
+++ ezzquad_library/models/book.py
@@ -0,0 +1,7 @@
+# _*_ coding: utf-8 _*_
+
+from odoo import models, fields, api
+
+class Book(models.Model):
+    _name = "ezzquad_lib.book"
+    _description = "Book info"
